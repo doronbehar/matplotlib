da508c21d4657c0be0500cf9f2bfc762d42e70bc
Doron Behar <doron.behar@gmail.com>
2024-07-18 12:29:39 +0300

    Add many qt related settings

--- flake.nix
+++ flake.nix
@@ -28,10 +28,31 @@
         ++ python.pkgs.matplotlib.buildInputs
         ;
         propagatedBuildInputs = [
+          # To test qt backend related scripts
+          python.pkgs.pyqt6
         ]
         ++ python.pkgs.matplotlib.propagatedBuildInputs
         ++ python.pkgs.matplotlib.dependencies
         ;
+        QT_PLUGIN_PATH = pkgs.lib.pipe [
+          pkgs.qt6.qtbase
+          pkgs.qt6.qtwayland
+        ] [
+          (map (p: "${pkgs.lib.getBin p}/${pkgs.qt6.qtbase.qtPluginPrefix}"))
+          (pkgs.lib.concatStringsSep ":")
+        ];
+        QT_QPA_PLATFORM="wayland";
+        XDG_DATA_DIRS = pkgs.lib.concatStringsSep ":" [
+          # So we'll be able to save figures from the plot dialog
+          "${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}"
+          "${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}"
+          # So pdf mime types and perhaps others could be detected by 'gio open'
+          # / xdg-open. TODO: Is this the best way to overcome this issue -
+          # manually every time we generate a devShell?
+          "${pkgs.shared-mime-info}/share"
+          "${pkgs.hicolor-icon-theme}/share"
+        ];
+        GDK_PIXBUF_MODULE_FILE = "${pkgs.librsvg}/${pkgs.gdk-pixbuf.moduleDir}.cache";
         # Install to this path with (in contrast to upstream's documentation):
         #
         # python -m pip install \
